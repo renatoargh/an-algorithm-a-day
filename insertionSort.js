2e0b970ea8c44c5737ec85227d296d55947b2520
Renato Gama <renatoargh@gmail.com>
2016-01-21 19:03:23 -0200

    Two more

---
+++ insertionSort.js
@@ -0,0 +1,14 @@
+function insertionSort(array, item) {
+  for (var i = 1; i < array.length; i++) {
+    var key = array[i],
+        j = i - 1;
+
+    while(j >= 0 && array[j] > key) {
+        array[j + 1] = array[j--];
+    }
+
+    array[j + 1] = key;
+  }
+
+  return array;
+}
